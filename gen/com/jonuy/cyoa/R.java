f8480075a4f0b36603ae032a6e0b9e1c986215c0
jonuy <jonxuy@gmail.com>
2012-12-06 02:00:07 -0500

    Story layouts changed to RelativeLayout, buttons locked to bottom of screen

--- gen/com/jonuy/cyoa/R.java
+++ gen/com/jonuy/cyoa/R.java
@@ -26,10 +26,10 @@ public final class R {
     public static final class id {
         public static final int btnStart=0x7f070005;
         public static final int choice_container=0x7f070004;
-        public static final int endButton=0x7f070003;
+        public static final int endButton=0x7f070001;
         public static final int header=0x7f070000;
-        public static final int image=0x7f070001;
-        public static final int text=0x7f070002;
+        public static final int image=0x7f070002;
+        public static final int text=0x7f070003;
     }
     public static final class layout {
         public static final int end_page=0x7f030000;
